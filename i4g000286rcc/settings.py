59f4999e401c9c47860c9b84787c810d90193c54
Moses ifeanyi <moses.ifeanyim@gmail.com>
2022-06-27 19:59:44 -0400

    update to key

--- i4g000286rcc/settings.py
+++ i4g000286rcc/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0%z2jdr-93h%^woqw=nnh*oc&)v6g^)jec3)r4g)mz!(4g9^4l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
